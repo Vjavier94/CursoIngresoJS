1ee4094ec35b2b253f63f713a8e79f9af997b66f
Vjavier94 <victorjaviertd@gmail.com>
2021-07-06 01:07:44 -0300

    tercer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Señor Tovar");
+	alert("Esto funciona de maravilla");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,14 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+
+	var nombre,mensaje;
+
+	Nombre=prompt("Ingresa su nombre");
+
+	mensaje="su nombe es:" + Nombre;
+
+	document.getElementById("txtIdNombre").value=mensaje;
+	
 }
 
